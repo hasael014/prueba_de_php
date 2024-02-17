d844e53b923dd84ee8f09ecfa034b38593cb9ff0
hasael014 <79614326+hasael014@users.noreply.github.com>
2024-02-16 19:09:50 -0600

    test de que funciona node en netlify
    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.get('/test', (req, res) => {
+app.get('/test1', (req, res) => {
   res.sendFile(path.join(__dirname+'/test.html'))
 })
 
